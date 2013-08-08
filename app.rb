f9f74150dbf89704eb34874a2941aaa78cc63a7b
Chloe Donegan <chloedonegan@gmail.com>
2013-08-08 09:05:40 +0100

    pictures

--- app.rb
+++ app.rb
@@ -9,16 +9,18 @@ get '/:to/:place/:from' do
     @message = "Come with me to"
     @place=params[:place].capitalize
     @from=params[:from].capitalize
-    erb :weregoingout
+    erb :weird
 end
 
 get '/frankscafe/:to/:when/:time/:from' do
     @to=params[:to].capitalize
     @when=params[:when].capitalize
-    @message = "I'm taking you to Frank's Cafe"
-    @message2 = "Meet me there at"
+    @place_url = "http://frankscafe.org.uk/"
+    @place = "Frank's Cafe"
+    @message2 = "Meet there at"
     @time=params[:time]
     @from=params[:from].capitalize
+    @picture="/images/FranksCafePeckham.jpg"
     erb :datenight
     
 end
@@ -26,9 +28,35 @@ end
 get '/bounce/:to/:when/:time/:from' do
     @to=params[:to].capitalize
     @when=params[:when].capitalize
-    @message = "I'm taking you to Bounce"
-    @message2 = "Meet me there at"
+    @place = "Bounce"
+    @place_url = "http://www.bouncepingpong.com/"
+    @message2 = "Meet there at"
     @time=params[:time]
     @from=params[:from].capitalize
+    @picture="/images/bounce.jpg"
+    erb :datenight
+end
+
+get '/sushisamba/:to/:when/:time/:from' do
+    @to=params[:to].capitalize
+    @when=params[:when].capitalize
+    @place = "Sushi Samba"
+    @place_url = "http://sushisamba.com/location/london/gallery/"
+    @message2 = "Meet there at"
+    @time=params[:time]
+    @from=params[:from].capitalize
+    @picture="/images/sushisamba.jpg"
+    erb :datenight
+end
+
+get '/thenightjar/:to/:when/:time/:from' do
+    @to=params[:to].capitalize
+    @when=params[:when].capitalize
+    @place = "The Night Jar"
+    @place_url = "http://www.barnightjar.com/"
+    @message2 = "Meet there at"
+    @time=params[:time]
+    @from=params[:from].capitalize
+    @picture="/images/nightjar.jpg"
     erb :datenight
 end
